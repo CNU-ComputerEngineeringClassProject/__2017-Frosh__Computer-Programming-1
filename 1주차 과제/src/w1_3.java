670a45ba0856334b46608df28c3711acca3e8910
Kuuuna98 <yunaghgh@naver.com>
2017-11-15 12:55:58 +0900

    first assignment

---
+++ 1주차 과제/src/w1_3.java
@@ -0,0 +1,46 @@
+
+public class w1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int lineNumber ;
+		
+		lineNumber = 1;
+		System.out.println(lineNumber + ". 우리 살아가는 일 속에");
+		lineNumber = 2;
+		System.out.println(lineNumber + ". 파도 치는 날, 바람 부는 날이");
+		lineNumber = 3;
+		System.out.println(lineNumber + ". 어찌 한 두 번이랴");
+		lineNumber = 4;
+		System.out.println(lineNumber + ". 그런 날은 조용히 닻을 내리고");
+		lineNumber = 5;
+		System.out.println(lineNumber + ". 오늘 일을 잠시라도");
+		lineNumber = 6;
+		System.out.println(lineNumber + ". 낮은 곳에 묻어 두어야 한다");
+		lineNumber = 7;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 8;
+		System.out.println(lineNumber + ". 우리 사랑하는 일 또한 그 같아서");
+		lineNumber = 9;
+		System.out.println(lineNumber + ". 파도 치는 날, 바람 부는 날은");
+		lineNumber = 10;
+		System.out.println(lineNumber + ". 높게 파도를 타지 않고");
+		lineNumber = 11;
+		System.out.println(lineNumber + ". 낮게 낮게 밀물져야 한다");
+		lineNumber = 12;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 13;
+		System.out.println(lineNumber + ". 사랑하는 이여");
+		lineNumber = 14;
+		System.out.println(lineNumber + ". 상처받지 않은 사랑이 어디 있으랴");
+		lineNumber = 15;
+		System.out.println(lineNumber + ". 추운 겨울 다 지내고");
+		lineNumber = 16;
+		System.out.println(lineNumber + ". 꽃 필 차례가 바로 그대 앞에 있다");
+		lineNumber = 17;
+		System.out.println(lineNumber + ". ");
+		lineNumber = 18;
+		System.out.println(lineNumber + ". -김종해-");
+	}
+
+}
